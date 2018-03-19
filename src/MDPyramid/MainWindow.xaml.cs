a7358a58080937d7a18f60750dc908b3217a8443
Andrius <andriusrdk@gmail.com>
2018-03-19 02:02:28 +0200

    Added some UI elements.

--- src/MDPyramid/MainWindow.xaml.cs
+++ src/MDPyramid/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace MDPyramid
 {
@@ -11,6 +14,60 @@ namespace MDPyramid
         public MainWindow()
         {
             InitializeComponent();
+            edInput.Text = Test<PrimitiveMethod>.QuestionStr;
+        }
+
+        private bool initialRefresh = true;
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+            if (initialRefresh)
+            {
+                initialRefresh = false;
+                RunPrimitive();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+                edSum.Clear();
+                edPath.Clear();
+                if (String.IsNullOrEmpty(edInput.Text)) return;
+                var m = new Method3();
+                int[] res = m.Run(edInput.Text);
+                edSum.Text = res.Sum().ToString();
+            }
+            finally
+            {
+                Mouse.OverrideCursor = null;
+            }
+        }
+
+        private void RunPrimitive()
+        {
+            try
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+                edSum.Clear();
+                edPath.Clear();
+                if (String.IsNullOrEmpty(edInput.Text)) return;
+                var m = new PrimitiveMethod();
+                int[] res = m.Run(edInput.Text);
+                edSum.Text = res.Sum().ToString();
+                edPath.Text = res.ToStringAggregate();
+            }
+            finally
+            {
+                Mouse.OverrideCursor = null;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            RunPrimitive();
         }
 
         //private void Button_Click(object sender, RoutedEventArgs e)
